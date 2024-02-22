20e50f206ab4b38ea2497c6d021dc79c65e66adf
harris829 <157516646+harris829@users.noreply.github.com>
2024-02-22 17:59:07 +0000

    <message about commit>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -28,12 +28,13 @@ $(function () {
      * Comment the lines out to remove the grid
      */
 
-    // for (let i = 100; i < canvas.width; i += 100) {
-    //   createPlatform(i, canvas.height, -1, -canvas.height);
-    // }
-    // for (let i = 100; i < canvas.height; i += 100) {
-    //   createPlatform(canvas.width, i, -canvas.width, -1);
-    // }
+    for (let i = 100; i < canvas.width; i += 100)
+     
+      createPlatform(i, canvas.height, -1, -canvas.height);
+     }
+     for (let i = 100; i < canvas.height; i += 100) {
+      createPlatform(canvas.width, i, -canvas.width, -1);
+    
 
     /////////////////////////////////////////////////
     //////////ONLY CHANGE BELOW THIS POINT///////////
@@ -42,8 +43,24 @@ $(function () {
     // TODO 1
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
-    // example usage: createPlatform(x,y,width,height)
-
+    // example usage: 
+    createPlatform(83,650,10,20)
+    createPlatform(306,650,50,20)
+    createPlatform(357,650,50,20)
+    createPlatform(400,475,60,20)
+    createPlatform(459,650,50,20)
+    createPlatform(580,610,400,20)
+    createPlatform(650,360,40,20)
+    createPlatform(940,360,50,20)
+    createPlatform(1358,700,50,20)
+    
+    
+    
+  
+    
+    
+    
+   
 
 
     
@@ -52,8 +69,9 @@ $(function () {
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
+    createCollectable("diamond", 410, 440,0, 20)
+    createCollectable("diamond", 650, 318,0, 20)
+    createCollectable("diamond", 1350, 680,0, 20)
 
 
     // TODO 3
@@ -61,7 +79,8 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
+    createCannon("left", 700, 10, 20, 45)
+    
 
 
 
