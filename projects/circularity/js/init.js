ddd5513e80abf1cd76bc0d4c94c001771060e4f2
harris829 <157516646+harris829@users.noreply.github.com>
2024-04-25 16:01:10 +0000

    add bouncing box

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -21,14 +21,13 @@ var init = function (window) {
         
         // TODO 1 : Declare and initialize our variables
 var circle ;
-var circles = []
+var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
 function drawCircle(){circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
 physikz.addRandomVelocity(circle, canvas, 2.5, 2.5);
 view.addChild(circle);
-circles.push(circle);};
-
+circles.push(circle);}
         // TODO 3 / 7 : Call the drawCircle() function 
         drawCircle();
         drawCircle();
@@ -47,11 +46,7 @@ circles.push(circle);};
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            physikz.updatePosition(1);
-            physikz.updatePosition(2);
-            physikz.updatePosition(3);
-            physikz.updatePosition(4);
-            physikz.updatePosition(5);
+            
           }
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
